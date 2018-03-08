43bc9d341a44b1eb3bb4d4c0c9dfb844629914f9
Alban de Crevoisier <albandecrevoisier@gmail.com>
2018-03-08 15:23:06 +0100

    Add CNF assignement - recursion does not work.

---
+++ logic0_CNF.pl
@@ -0,0 +1,70 @@
+/*---------------------------------------------------------------*/
+/* Telecom ParisTech                                             */
+/*                                                   Dep. INFRES */
+/* Logic & Knowledge representation - Dessalles 2016             */
+/* http://teaching.dessalles.fr/LRC                              */
+/*---------------------------------------------------------------*/
+
+
+:-op(140, fy, -).        
+:-op(160,xfy, [and, or, imp, impinv, nand, nor, nonimp, equiv, nonimpinv]).
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Conjunctive normal form %
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+/* table for unary, alpha and beta formulas */
+
+components(- -X, X, _, unary).
+components(X and Y, X, Y, alpha).
+components(-(X or Y), -X, -Y, alpha).
+components(X or Y, X, Y, beta).
+components(-(X and Y), -X, -Y, beta).
+components(X imp Y, -X, Y, beta).
+components(-(X imp Y), X, -Y, alpha).
+components(X impinv Y, X, -Y, beta).
+components(-(X impinv Y), -X, Y, alpha).
+components(X nand Y, -X, -Y, beta).
+components(-(X nand Y), X, Y, alpha).
+components(X nor Y, -X, -Y, alpha).
+components(-(X nor Y), X, Y, beta).
+components(X nonimp Y, X, -Y, alpha).
+components(-(X nonimp Y), -X, Y, beta).
+components(X nonimpinv Y, -X, Y, alpha).
+components(-(X nonimpinv Y), X, -Y, beta).
+
+
+% Predicate cnf puts more elementary processing together
+cnf(Conjunction, NewConjunction) :-
+	oneStep(Conjunction, C1),
+	cnf(C1, NewConjunction).
+cnf(C, C).
+
+
+% Predicate oneStep performs one elementary processing
+oneStep([-(-X) | R], [X | R]).
+oneStep([C|S], [[A,B|R]| S]) :-
+	components(P, A, B, beta),
+	remove(P, C, R).
+oneStep([P|S], [[A|S]|[B|S]]) :-
+	components(P, A, B, alpha).
+oneStep([ F | Rest], [ F | New_Rest ]) :-
+	% nothing left to do on F
+	oneStep(Rest, New_Rest).
+
+
+/*------------------------------------------------*/
+/* Auxiliary predicates                           */
+/*------------------------------------------------*/
+
+/* remove does as select, but removes all occurrences of X */
+remove(X, L, NL) :-
+    member(X,L),
+    remove1(X, L, NL).
+remove1(X, L, L) :-
+    not(member(X,L)).
+remove1(X, L, NL) :-
+	select(X, L, L1),   % available in SWI-Prolog
+	remove1(X, L1, NL).
+
